9ba3b31c5dca3d7cbb28cdfc68c5c7c1081e1415
UlianaGritchina <ugritchina@icloud.com>
2022-04-01 12:31:43 +0300

    Initial Commit

---
+++ Involta/InvoltaApp.swift
@@ -0,0 +1,17 @@
+//
+//  InvoltaApp.swift
+//  Involta
+//
+//  Created by Ульяна Гритчина on 01.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct InvoltaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
